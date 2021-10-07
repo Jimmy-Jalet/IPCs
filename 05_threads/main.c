8fc6390766a9cc67781faa048262178b5b891557
Jimmy Jalet <jaletjimmy@gmail.com>
2021-10-07 11:12:07 +0200

    fin du td

--- 05_threads/main.c
+++ 05_threads/main.c
@@ -12,75 +12,118 @@ typedef struct {
     int v2;
     int v3;
 } laStruct;
-//zone de partage
+
+//Zone de partage
 laStruct partage;
+//mutex
+pthread_mutex_t verrou;
 
 void *thread1(void *arg) {
-    printf("dans le thread1 mon tid est :%d\n", syscall(SYS_gettid));
-    printf("mise a jour de v1 avec la valeur 10\n");
-    partage.v1 = 10;
+    int *ptr = (int *) arg;
+    printf("dans le thread 1 mon pid est %ld\n", syscall(SYS_gettid));
+    printf("mise à jour de v1 avec la valeur %d\n", *ptr);
+    partage.v1 = *ptr;
+
     pthread_exit(NULL);
 }
 
 void *thread2(void *arg) {
-    printf("dans le thread1 mon tid est :%d\n", syscall(SYS_gettid));
-    printf("mise a jour de v2 avec la valeur 20\n");
-    partage.v2 = 20;
+    int *ptr = (int *) arg;
+    printf("dans le thread 2 mon pid est %ld\n", syscall(SYS_gettid));
+    printf("mise à jour de v1 avec la valeur %d\n", *ptr);
+    partage.v2 = *ptr;
     pthread_exit(NULL);
 }
 
 void *thread3(void *arg) {
+    int *ptr = (int *) arg;
+    printf("dans le thread 3 mon pid est %ld\n", syscall(SYS_gettid));
+    printf("mise à jour de v1 avec la valeur %d\n", *ptr);
+    partage.v3 = ptr;
+    pthread_exit(NULL);
+}/
 
-    printf("dans le thread1 mon tid est :%d\n", syscall(SYS_gettid));
-    printf("mise a jour de v3 avec la valeur 30\n");
-    partage.v3 = 30;
+void *threadMulti(void *arg) {
+    laStruct *ptr = (laStruct *) arg;
+    printf("dans le threadMulti mon pid est %ld\n", syscall(SYS_gettid));
+    printf("mise à jour de v1,v2,v3 avec les valeurs %d,%d,%d\n", ptr->v1, ptr->v2, ptr->v3);
+    //Section Critique
+    pthread_mutex_lock(&verrou);
+    partage.v1 = ptr->v1;
+    sleep(3 - ptr->v1);
+    partage.v2 = ptr->v2;
+    partage.v3 = ptr->v3;
+    pthread_mutex_unlock(&verrou);
+    //Fin section critique
     pthread_exit(NULL);
 }
 
 void affichePartage() {
-    printf("--------------------\n");
+    pthread_mutexlock(&verrou);
+    printf("__\n");
     printf("v1 = %d\n", partage.v1);
     printf("v2 = %d\n", partage.v2);
     printf("v3 = %d\n", partage.v3);
+    pthread_mutex_unlock(&verrou);
 }
-
-int main(int argc, char** argv) {
-
-    pthread_t T1, T2, T3;
+pthread_t t1, t2, t3;
     int ret;
     void *thread_result;
+    laStruct param1, param2, param3;
+    param1.v1 = 1;
+    param1.v2 = 1;
+    param1.v3 = 1;
+    //Initialisation du mutex
+    ret= pthread_mutex_init(&verrou, NULL);
+    if(ret!=0)
+    {
+        perror("Pb Mutex Initialisation");
+        exit(EXIT_FAILURE);
+    }
     affichePartage();
-    ret = pthread_create(&T1, NULL, thread1, NULL);
+    //creation du thread 1
+
+    ret = pthread_create(&t1, NULL, threadMulti, (void *) &param1);
     if (ret != 0) {
         perror("Thread creation failed");
         exit(EXIT_FAILURE);
     }
     affichePartage();
-    ret = pthread_create(&T2, NULL, thread2, NULL);
+    //creation du thread 2
+    param2.v1 = 2;
+    param2.v2 = 2;
+    param2.v3 = 2;
+    ret = pthread_create(&t2, NULL, threadMulti, (void *) &param2);
     if (ret != 0) {
         perror("Thread creation failed");
         exit(EXIT_FAILURE);
     }
     affichePartage();
-    ret = pthread_create(&T3, NULL, thread3, NULL);
+    //creation du thread 3
+    param3.v1 = 3;
+    param3.v2 = 3;
+    param3.v3 = 3;
+    ret = pthread_create(&t3, NULL, threadMulti, (void *) &param3);
     if (ret != 0) {
         perror("Thread creation failed");
         exit(EXIT_FAILURE);
     }
     affichePartage();
-    ret = pthread_join(T1, &thread_result);
+
+    //attendre fin du thread 1
+    ret = pthread_join(t1, &thread_result);
     if (ret != 0) {
         perror("Thread join a foir... echoué");
         exit(EXIT_FAILURE);
     }
-    affichePartage();
-    ret = pthread_join(T2, &thread_result);
+    //attendre fin du thread 2
+    ret = pthread_join(t2, &thread_result);
     if (ret != 0) {
         perror("Thread join a foir... echoué");
         exit(EXIT_FAILURE);
     }
-    affichePartage();
-    ret = pthread_join(T3, &thread_result);
+    //attendre fin du thread 3
+    ret = pthread_join(t3, &thread_result);
     if (ret != 0) {
         perror("Thread join a foir... echoué");
         exit(EXIT_FAILURE);
